3de387151d563042a02ee7da837192d5d60960cb
guiyom <guiyom@PC2>
2020-03-13 12:19:13 +0100

    2 simple heuristics + group heuristic + map helpers improved (WARN: signature changed)

---
+++ vampires_vs_direwolves/minmax/group_heuristics.py
@@ -0,0 +1,29 @@
+from typing import List, Type
+
+from common.models import Species
+from game_management.abstract_game_map import AbstractGameMap
+from minmax.abstract_heuristic import AbstractHeuristic
+
+
+class HeuristicGroup(AbstractHeuristic):
+
+    def __init__(self, heuristics: List[Type[AbstractHeuristic]], weight=1000):
+        """
+
+        :param heuristics: ordered list of heuristic classes
+        :param weight: relative weight of the order in the list:
+        1 for each heuristic has the same weight,
+        1000 for the heuristic has a weight proportional to 1000 times power its opposite order (len - order)
+        """
+        super().__init__()
+        self._heuristics = heuristics
+        self._weight = weight
+
+    def evaluate(self, game_map: AbstractGameMap, specie: Species):
+        """ Evaluate the current map and return a number to score if it's in favour of the specie
+        """
+        heuristic_result = 0
+        for i, heuristic in enumerate(self._heuristics):
+            coef = self._weight ** (len(self._heuristics) - i) or 1
+            heuristic_result += coef * heuristic().evaluate(game_map, specie)
+        return heuristic_result
