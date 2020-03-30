55b1d8d7e38775b0d07f113818a232ad99ec7668
Quentin Nativel <qmnativel@gmail.com>
2020-03-30 17:49:43 +0200

    refactor alphabeta search

--- vampires_vs_direwolves/alphabeta/alphabeta.py
+++ vampires_vs_direwolves/alphabeta/alphabeta.py
@@ -1,7 +1,7 @@
 """
-code an alphabeta algorithm
+Inspired from http:aipython.org
 """
-import time
+
 from copy import copy
 from typing import Type
 
@@ -14,129 +14,80 @@ from game_management.abstract_game_map import AbstractGameMap
 from game_management.game_map import compute_new_board
 
 
-def propagate(alpha_betas, val):
-    pass
-
-
-class AlphaBetaSearch:
+class AlphaBetaSearch2:
 
     def __init__(self, possible_moves_computer: Type[AbstractPossibleMovesComputer],
                  heuristic: Type[AbstractHeuristic], depth: int):
         self.move_computer = possible_moves_computer()
         self.heuristic = heuristic()
-        self.depth = depth
+        self.max_depth = depth
+        self.specie = None
+        self.other_specie = None
 
     def compute(self, game_map: AbstractGameMap, specie: Species):
-        logger.debug('START SEARCH!!!!!!!!!!!!!!!!!!!!!')
-        # logger.debug('NEWWWWWW MAPPP', compute_new_board(
-        #     game_map, (4, 5, 3, 4, 4))._map_table)
-        best_move = None
-        best_val = -1e6 - 1
-
-        other_specie = Species.VAMPIRE if specie == Species.WEREWOLF else Species.WEREWOLF
-        logger.debug(
-            f"SPECC We play the {specie.name}, other plays {other_specie.name}")
-        states = [{
+        self.specie = specie
+        self.other_specie = Species.VAMPIRE if specie == Species.WEREWOLF else Species.WEREWOLF
+        start_node = {
             'board': game_map,
-            'moves': self.move_computer.compute(game_map, specie),
-            'alpha': -1e6,
-            'beta': 1e6
-        }]
-        # print('MOVES STATE 0', states[0]['moves'])
-        # time.sleep(2)
+            'max': True,
+            'mv': None
+        }
 
-        while states:
-            s = states[-1]
-            depth = len(states)
-            # logger.debug(f'ENTER WHILEE!!!!!!!!!!!!! {depth}')
-            if depth < 2:
-                logger.debug(" ".join(['ALPHABETA', str(
-                    s['alpha']), str(s['beta']), str(depth)]))
-            # state is a leaf
-            if depth >= self.depth or s['board'].game_over()[0]:
-                val = self.heuristic.evaluate(s['board'], specie)
-                logger.debug(" ".join(['LEAFFFFFFF', 'VAL', str(val),
-                                       'OVER',
-                                       str(s['board'].game_over()[0])]))
-                # logger.debug(s['board']._map_table)
+        score, path = self.minmax_alpha_beta(start_node, -1e6 - 1, 1e6 + 1)
+        #print('PATHHHHH', path)
+        move = path[0]['mv']
+        return [move], score
 
-                states.pop()
-                if depth % 2 and states:  # maximize
-                    states[-1]['beta'] = min(s['beta'], val)
-                    states[-1]['alpha'] = max(s['alpha'], val)
-                elif states:  # minimize
-                    states[-1]['alpha'] = max(s['alpha'], val)
+    def is_leaf(self, node, depth):
+        over = node['board'].game_over()[0]
+        return over or depth >= self.max_depth
 
-                if depth == 2:
-
-                    # logger.debug(
-                    #     f"Update Best Moveeeeeeeeeee, WIN/LOOSE: {best_move} {str(s['mv'])}")
-                    # logger.debug(f"best_val {str(val)}")
-
-                    if val > best_val:  # update best move
-                        best_move = s['mv']
-                        best_val = val
-                    # time.sleep(5)
-
-            elif s['alpha'] > s['beta']:  # prune the tree
-                # logger.debug('PRUNINNNNNNNG')
-                states.pop()
-                if depth == 2:
-                    # logger.debug(
-                    #     f"Update Best Moveeeeeeeeeee: {best_move} {s['mv']}")
-                    # logger.debug(f"best_val {str(best_val)} {s['beta']}")
-                    if s['beta'] > best_val:  # update best move
-                        best_move = s['mv']
-                        best_val = s['beta']
-                    # time.sleep(5)
-
-            elif not s['moves']:  # go back up in the tree
-                # logger.debug('GO BACKKKK UPPPPP')
-                states.pop()
-                if states:
-                    if depth % 2:  # minimize
-                        states[-1]['beta'] = min(states[-1]
-                                                 ['beta'], s['alpha'])
-                    else:
-
-                        states[-1]['alpha'] = max(states[-1]
-                                                  ['alpha'], s['beta'])
-                    # logger.debug(
-                    #     f"UPPPPPP {depth - 1} {states[-1]['alpha']} {states[-1]['beta']}")
-                if depth == 2:
-                    # logger.debug(
-                    #     f"Update Best Moveeeeeeeeeee: {best_move} {str(s['mv'])}")
-                    # logger.debug(f"best_val {str(best_val)} {str(s['beta'])}")
-                    if s['beta'] > best_val:  # update best move
-                        best_move = s['mv']
-                        best_val = s['beta']
-                    # time.sleep(5)
-
-            else:  # continue exploring the tree
-                move = s['moves'].pop(0)
-                # logger.debug(f'EXPLOREEEEEEEE {move}')
-                new_board = compute_new_board(s['board'], move)
-                new_moves = []
-                if depth + 1 < self.depth:
-                    if depth % 2:  # it will be our turn
-                        # logger.debug('other playyys')
-                        new_moves = self.move_computer.compute(
-                            new_board, other_specie)
-                    else:
-                        # logger.debug('We plaayy')
-                        new_moves = self.move_computer.compute(
-                            new_board, specie)
-
-                new_state = {
-                    'board': new_board,
-                    'moves': new_moves,
-                    'alpha': s['alpha'],
-                    'beta': s['beta']
+    def minmax_alpha_beta(self, node, alpha, beta, depth=0):
+        """
+            node is a Node, alpha and beta are cutoffs, depth is the depth
+            returns value, path
+            where path is a sequence of nodes that results in the value
+        """
+        best = []
+        if self.is_leaf(node, depth):
+            val = self.heuristic.evaluate(node['board'], self.specie)
+            return val, [node]
+        elif node['max']:
+            moves = self.move_computer.compute(
+                node['board'], self.specie)
+            for move in moves:
+                # create new node
+                child = {
+                    'board': compute_new_board(node['board'], move),
+                    'max': False,
+                    'mv': move
                 }
-                if depth == 1:
-                    logger.debug(f'Tesssssssssst Move {str(move)}')
-                    new_state['mv'] = move,  # remeber the move
-                states.append(new_state)
-        logger.info(
-            f'AlphaBeta result: move: {str(best_move)}  value: {str(best_val)}')
-        return best_move, best_val
+                score, path = self.minmax_alpha_beta(
+                    child, alpha, beta, depth+1)
+                if score >= beta:  # beta pruning
+                    b = [child] + path
+                    return score, b
+                if score > alpha:
+                    alpha = score
+                    best = [child] + path
+            return alpha, best
+
+        else:
+            moves = self.move_computer.compute(
+                node['board'], self.other_specie)
+            for move in moves:
+                # create new node
+                child = {
+                    'board': compute_new_board(node['board'], move),
+                    'max': True,
+                    'mv': move
+                }
+                score, path = self.minmax_alpha_beta(
+                    child, alpha, beta, depth+1)
+                if score <= alpha:  # alpha pruning
+                    b = [child] + path
+                    return score, b
+                if score < beta:
+                    beta = score
+                    best = [child] + path
+            return beta, best
--- vampires_vs_direwolves/boutchou/alpha_beta_ai.py
+++ vampires_vs_direwolves/boutchou/alpha_beta_ai.py
@@ -1,33 +1,26 @@
-<<<<<<< HEAD
+import time
+
 from alphabeta.abstract_heuristic import NumberAndDistanceHeuristic
 from alphabeta.abstract_possible_moves_computer import SimpleMoveComputer
 from alphabeta.alphabeta import AlphaBetaSearch
+from alphabeta.alphabeta2 import AlphaBetaSearch2
 from boutchou.abstract_ai import AbstractAI
-=======
-from boutchou.abstract_ai import AbstractAI
-from alphabeta.abstract_heuristic import NumberAndDistanceHeuristic
-from alphabeta.abstract_possible_moves_computer import SimpleMoveComputer
-from alphabeta.alphabeta import AlphaBetaSearch
->>>>>>> master
 
 
 class AlphaBetaAI(AbstractAI):
 
     def __init__(self):
-        self.search = AlphaBetaSearch(possible_moves_computer=SimpleMoveComputer,
-                                      heuristic=NumberAndDistanceHeuristic,
-<<<<<<< HEAD
-                                      depth=5)
-=======
-                                      depth=3)
->>>>>>> master
         super().__init__()
+
+        self.search = AlphaBetaSearch2(
+            possible_moves_computer=SimpleMoveComputer,
+            heuristic=NumberAndDistanceHeuristic,
+            depth=5,
+        )
 
     def generate_move(self):
         move, blaaaaaaaa = self.search.compute(self._map, self._species)
-<<<<<<< HEAD
-        #print('MOVE!!!!!!!!!!!!!', move, blaaaaaaaa, type(move))
-=======
         print('MOVE!!!!!!!!!!!!!', move, blaaaaaaaa, type(move))
->>>>>>> master
-        return [move[0]]
+        time.sleep(2)
+        # return [move[0]]
+        return move
